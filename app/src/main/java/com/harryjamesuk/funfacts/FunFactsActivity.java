821ad6befb98542ceaa6b0592ee0e34a06118ee8
harryjamesuk <harryjamesuk+github@gmail.com>
2014-12-22 16:52:51 +0000

    Deleted test code
    Some test code made its way through to the last commit :ok_woman:

--- app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
@@ -14,8 +14,6 @@ public class FunFactsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_fun_facts);
     }
 
-    String myName = "Harry";
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
