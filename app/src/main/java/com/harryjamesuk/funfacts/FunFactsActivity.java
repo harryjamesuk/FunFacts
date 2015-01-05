9caf467fcfdfb7d662111574702ccd552e29483e
harryjamesuk <harryjamesuk+github@gmail.com>
2015-01-05 19:50:19 +0000

    Refactored code
    Added Treehouse to dictionary.
    Created new FactBook class to deal with anything relating to handling
    the facts.
    Removed some code from FunFactsActivity class so that it will only have
    code relating to handling the screen layout.

--- app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
@@ -25,61 +25,11 @@ public class FunFactsActivity extends ActionBarActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built.",
-                        "Treehouse is not actually in a tree." };
-
-                // The button was clicked so update the factLabel with a new fact.
-                String fact = "";
-                // We'll randomly select a fact
-                Random randomGenerator = new Random(); // Construct a new random number generator.
-                int randomNumber = randomGenerator.nextInt(facts.length);
-
-                /* Convert the randomNumber to a text fact
-                 * 0 = Ants stretch when they wake up in the morning.
-                 * 1 = Ostriches can run faster than horses.
-                 * 2 = Olympic gold medals are actually made mostly of silver.
-                 */
-
-                fact = facts[randomNumber];
-
                 // Update the label with our dynamic fact
-                factLabel.setText(fact);
+                factLabel.setText("");
             }
         };
 
         showFactButton.setOnClickListener(listener);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
