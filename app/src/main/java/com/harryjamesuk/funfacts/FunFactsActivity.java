00e49d4e481ddc89fb6f4922b4a585d1372150ce
harryjamesuk <harryjamesuk+github@gmail.com>
2015-01-07 22:18:43 +0000

    Background colour is set to red upon clicking button
    Gave RelativeLayout id of relativeLayout (I know, I'm inventive).
    
    Created findViewById link to relativeLayout.
    Set background colour of relativeLayout to red once the button is
    tapped.

--- app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/harryjamesuk/funfacts/FunFactsActivity.java
@@ -1,11 +1,13 @@
 package com.harryjamesuk.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -23,6 +25,7 @@ public class FunFactsActivity extends ActionBarActivity {
         // Declare our view variables and assign them the views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -32,6 +35,7 @@ public class FunFactsActivity extends ActionBarActivity {
 
                 // Update the label with our dynamic fact
                 factLabel.setText(fact);
+                relativeLayout.setBackgroundColor(Color.RED);
             }
         };
 
